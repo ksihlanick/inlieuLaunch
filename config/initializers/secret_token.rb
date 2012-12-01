c6c5d9aaa079a508795334f3d4b0878fe841374a
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-01 14:48:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InlieuLaunch::Application.config.secret_token = '9724f3d4c0a135c62a6a1d081013d88ad3b53202beb2eed91d00ad3b6a508fb0d1429e92e225675d85192eac574ebfdd7944245f4238175c5013d453b8799c30'
